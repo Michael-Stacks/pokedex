4d19a98ee231c9e909336886e68460419c0cbb64
jabir756 <jabirkagone6@gmail>
2024-09-08 18:52:54 -0400

    update index.js and css

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 "use strict"
 
-let path = 'assets/pokemon-img/'
-const main = document.querySelector('main')
+let path = "assets/pokemon-img/"
+const pokemonContainer = document.querySelector("main")
 
 for (let i = 1; i <= 151; i++) {
-  const img = document.createElement('img')
-  const div = document.createElement('div')
-  const section = document.createElement('section')
-  const p = document.createElement('p')
+  const image = document.createElement('img')
+  const imageContainer = document.createElement('div')
+  const card = document.createElement('div')
+  const pokemonInfos = document.createElement('p')
 
   if (i < 10) {
     // Pour les nombres à un chiffre
-    img.src = `${path}pokemon_00${i}.png`    
+    image.src = `${path}pokemon_00${i}.png`    
   } else if (i < 100) {
     // Pour les nombres à deux chiffres
-    img.src = `${path}pokemon_0${i}.png`
+    image.src = `${path}pokemon_0${i}.png`
   } else {
     // Pour les nombres à trois chiffres
-    img.src = `${path}pokemon_${i}.png`
+    image.src = `${path}pokemon_${i}.png`
   }
 
-
-  img.loading = 'lazy'
-  div.appendChild(img)
-  section.appendChild(div,p)
-  main.appendChild(section)
+  image.loading = 'lazy'
+  pokemonInfos.innerText = `Pokémon #${i}`
+  imageContainer.append(image)
+  imageContainer.className = "image-container"
+  card.append(imageContainer, pokemonInfos)
+  card.className = "card"
+  pokemonContainer.append(card)
 }
+
+/*Section click*/
+
+
+
